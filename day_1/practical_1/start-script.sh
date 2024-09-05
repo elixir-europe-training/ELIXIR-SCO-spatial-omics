533fcda0fdf563c395b080f43499ea7e114e364f
Rasool Saghaleyni <rasool.saghaleyni@gmail.com>
2024-09-05 11:38:16 +0200

    Test workflow

--- day_1/practical_1/start-script.sh
+++ day_1/practical_1/start-script.sh
@@ -2,4 +2,4 @@
 # Activate the conda environment
 source /opt/conda/bin/activate imaging_based_data_analysis_env
 # Start the Jupyter Notebook server
-exec jupyter notebook --allow-root --notebook-dir=/home/jovyan/workdir
+exec jupyter notebook --allow-root --notebook-dir=/home/jovyan
