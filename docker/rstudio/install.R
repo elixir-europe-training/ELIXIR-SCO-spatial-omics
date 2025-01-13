17db574fa9e291570e0b244b9bbac628b9b987b9
Mark Robinson <mark.robinson@mls.uzh.ch>
2025-01-13 09:24:12 +0100

    separate docker

--- docker/rstudio/install.R
+++
@@ -1,6 +0,0 @@
-pkgs <- c('SpatialExperiment', 'spatstat.geom', 'spatstat.explore', 
-          'dplyr', 'ggplot2', 'patchwork', 'reshape2', 'Voyager', 
-          'SpatialFeatureExperiment', 'SFEData', 'spdep', 'sf', 
-          'stringr', 'tidyr','magrittr','scater','BiocStyle')
-BiocManager::install(pkgs)
-devtools::install_github("mjemons/spatialFDA")
